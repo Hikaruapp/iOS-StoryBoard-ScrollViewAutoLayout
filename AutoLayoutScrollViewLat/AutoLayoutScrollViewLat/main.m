ceb44f78661f68aedc78a94a16d84860b5f0c6f1
Hikaruapp <info-iphone@hikaruapp.jpn.com>
2016-07-25 21:44:36 +0900

    first commit

---
+++ AutoLayoutScrollViewLat/AutoLayoutScrollViewLat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AutoLayoutScrollViewLat
+//
+//  Created by Yanase Yuji on 2016/07/25.
+//  Copyright © 2016年 hikaruApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
